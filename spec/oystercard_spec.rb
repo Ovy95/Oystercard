0c1e9883cd839f4c3944d912c8db3d297b21aacd
Ovy95 <jackoverton21@hotmail.com>
2020-01-28 13:40:09 +0000

    Step 5

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,4 +1,17 @@
+require 'Oystercard'
 
 describe Oystercard do
 
+    it "(1) Has a balance of zero" do
+    expect(subject.balance).to eq(0)
+    end
+
+    describe '#top_up' do
+    it "(2) Is expected to top up the balance" do
+    expect(subject).to respond_to(:top_up).with(1).argument
+
+    it " tops up the balance" do
+        expect(subject.top_up(1).to change(subject.balance).by 1
+    end
+    end
 end 
