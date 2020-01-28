df4d9d73744d33950adc274fa6d0a92b7b114190
Ovy95 <jackoverton21@hotmail.com>
2020-01-28 16:32:54 +0000

    (8) Touch in and Out method added

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,10 +1,32 @@
 class Oystercard
-    attr_reader :balance
+    attr_reader :balance 
+
+    MAX_BALANCE = 90
     def initialize
         @balance = 0
+        @in_journey = false
     end
 
     def top_up(amount)
 
+      raise "Max balance of #{MAX_BALANCE} exceeded" if (@balance + amount) > MAX_BALANCE
+      @balance = @balance + amount
     end
+
+    def deduct(amount)
+      @balance = @balance - amount
+    end
+
+    def in_journey?
+      @in_journey
+    end
+
+    def touch_in
+      @in_journey = true
+    end
+
+    def touch_out
+      @in_journey = false
+    end
+
 end 
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,16 +2,54 @@ require 'Oystercard'
 
 describe Oystercard do
 
-    it "(1) Has a balance of zero" do
+  it "(1) Has a balance of zero" do
     expect(subject.balance).to eq(0)
-    end
+  end
 
-    describe '#top_up' do
+  describe '#top_up' do
     it "(2) Is expected to top up the balance" do
-    expect(subject).to respond_to(:top_up).with(1).argument
+      expect(subject).to respond_to(:top_up).with(1).argument 
+    end
 
-    it " tops up the balance" do
-        expect(subject.top_up(1).to change(subject.balance).by 1
+    it "(3) tops up the balance" do
+      expect{ subject.top_up 1 }.to change{ subject.balance }.by 1
     end
+
+    it "(4) raises an error is the balance is more then 90" do 
+      max_balance = Oystercard::MAX_BALANCE
+      subject.top_up(max_balance)
+      expect {subject.top_up(1)}.to raise_error "Max balance of #{max_balance} exceeded"
     end
-end 
+
+  end
+
+  describe "#deduct" do 
+    it " takes the amount checkes entered amount" do 
+      expect(subject).to respond_to(:deduct).with(1).argument 
+    end
+
+    it "deducts the fare from the balance " do 
+      expect{ subject.deduct 1 }.to change{ subject.balance }.by -1
+    end
+  end
+
+  describe "#In journey" do
+    it " Initally not in journey" do 
+      expect(subject).to_not be_in_journey
+    end 
+
+    it "can touch in " do 
+      subject.touch_in
+      expect(subject).to be_in_journey
+    end
+
+    it "can touch out " do 
+      subject.touch_in
+      subject.touch_out
+      expect(subject).to_not be_in_journey
+    end
+
+  end
+
+
+end
